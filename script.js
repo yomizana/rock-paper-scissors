a377219234d6400dae69dcec479230343552747f
Tomás Caraballo (Yomi) <yomizana@gmail.com>
2022-03-22 07:35:21 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const playerSelection = 'Paper';
+const computerSelection = computerPlay();
+
 function computerPlay() {
     let randomNumber = Math.floor((Math.random() * 100));
 
@@ -6,4 +9,28 @@ function computerPlay() {
     return 'Rock';
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    let winStatement = `You Win! ${playerSelection} beats ${computerSelection}.`;
+    let loseStatement = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    let tieStatement = `It's a tie!`;
+
+    if (playerSelection == 'Rock') {
+        if (computerSelection == 'Scissors') return winStatement;
+        if (computerSelection == 'Paper') return loseStatement;
+        return tieStatement;
+    } 
+    
+    if (playerSelection == 'Paper') {
+        if (computerSelection == 'Rock') return winStatement;
+        if (computerSelection == 'Scissors') return loseStatement;
+        return tieStatement;
+    } 
+    
+    if (playerSelection == 'Scissors') {
+        if (computerSelection == 'Paper') return winStatement;
+        if (computerSelection == 'Rock') return loseStatement;
+        return tieStatement;
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
