8f331e17d07c662f02c6fa9ef2fa9220290a97f8
Tomás Caraballo (Yomi) <yomizana@gmail.com>
2022-03-28 15:27:20 -0400

    Remove calls to functions that no longer exist

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function computerChoice() {
     let randomNumber = Math.floor((Math.random() * 3));
 
-    if (randomNumber = 0) return 'rock';
-    if (randomNumber = 1) return 'paper';
-    return 'scissors';
+    if (randomNumber = 0) return 'Rock';
+    if (randomNumber = 1) return 'Paper';
+    return 'Scissors';
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'invalid input') return 'Invalid input';
-
     console.log(playerSelection, computerSelection);
 
     if (playerSelection == 'Rock') {
@@ -33,9 +31,3 @@ function playRound(playerSelection, computerSelection) {
 let rockButton = document.querySelector('#rock');
 let paperButton = document.querySelector('#paper');
 let scissorsButton = document.querySelector('#scissors');
-
-let compchoice = 'Rock';
-
-rockButton.addEventListener('click', () => game('Rock'));
-paperButton.addEventListener('click', () => game('Paper'));
-scissorsButton.addEventListener('click', () => game('Scissors'));
